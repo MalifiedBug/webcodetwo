c28708d4e0c15f51fc3e10d3dcdbe3a3853cc203
omkar lolge <omkarlolge119.ol@gmail.com>
2022-11-18 17:08:22 +0530

    changes

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -7,7 +7,7 @@ export default function Cart() {
   return (
     <div>
       <h1>Cart page</h1>
-      <a href="https://pages.razorpay.com/pl_KgRY5jcrF44g1O/view" alt="new-page" target="_blank">Razorpay Payment</a>
+      <a href="https://pages.razorpay.com/pl_KgRY5jcrF44g1O/view" alt="new-page" target="_blank" rel="noopener noreferrer">Razorpay Payment</a>
     </div>
   );
 }
